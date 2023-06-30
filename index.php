985538f1488422ca11ee790546c8c7282548a41d
ashcbrd <99721216+EjDadivas@users.noreply.github.com>
2023-06-30 16:55:37 +0800

    Updated

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                             </div>
                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
-                                    <form action="validate-login.php" method="POST">
+                                    <form action="index.php" method="POST">
                                         <div class="d-flex align-items-center mb-3 pb-1">
                                             <span class="h1 fw-bold mb-0">Library Management System</span>
                                         </div>
@@ -57,3 +57,30 @@
 </body>
 
 </html>
+
+<?php
+include('dbconnect.php');
+if (!isset($_POST['username'], $_POST['password'])) {
+    echo ' <script> Please fill both the username and password fields! </script>';
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username == "" || $password == "") {
+    echo "<script> Please fill both the username and password fields! </script>";
+    exit();
+}
+$Query = "SELECT admin_id FROM admin WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $Query);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
+if ($count == 1) {
+    echo "<script>
+    window.location.href='app/book/book.php'</script>";
+} else {
+    echo "<script> alert('Login Failed. Check your email or password') 
+    window.location.href='index.php'
+    </script>";
+}
+?>
